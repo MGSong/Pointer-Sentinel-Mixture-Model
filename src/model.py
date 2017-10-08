2908204e35624dc40b22459678744b56ff2c60d0
yzh119 <expye@outlook.com>
2017-10-08 14:49:15 +0000

    reimplement the initialization part

--- src/model.py
+++ src/model.py
@@ -14,10 +14,20 @@ class PSMM(nn.Module):
         self.affine1 = nn.Linear(hidden_size, vocab_size)
         self.affine2 = nn.Linear(hidden_size, hidden_size)
         self.rnn = nn.LSTMCell(hidden_size, hidden_size)
-        self.sentinel_vector = Variable(torch.rand(hidden_size, 1), requires_grad=True)
+        self.sentinel_vector = Variable(torch.zeros(hidden_size, 1), requires_grad=True)
         if self.use_cuda:
             self.sentinel_vector = self.sentinel_vector.cuda()
+        self.init_weight()
 
+    def init_weight(self):
+        init_range = 0.1
+        self.embed.weight.data.uniform_(-init_range, init_range)
+        self.affine1.weight.data.uniform_(-init_range, init_range)
+        self.affine1.bias.data.fill_(0)
+        self.affine2.weight.data.uniform_(-init_range, init_range)
+        self.affine2.bias.data.fill_(0)
+        self.sentinel_vector.data.uniform_(-init_range, init_range)
+    
     """
         Input size:
         length * batch_size
@@ -53,7 +63,6 @@ class PSMM(nn.Module):
             a = F.softmax(z.transpose(0, 1)).transpose(0, 1)
             prefix_matrix = cumulate_matrix[:step + 1]
             p_ptr = torch.sum(Variable(prefix_matrix) * a[:-1].unsqueeze(2).expand_as(prefix_matrix), 0).squeeze(0)
-
             output = self.affine1(hidden)
             p_vocab = F.softmax(output)
             p = p_ptr + p_vocab * a[-1].unsqueeze(1).expand_as(p_vocab)
